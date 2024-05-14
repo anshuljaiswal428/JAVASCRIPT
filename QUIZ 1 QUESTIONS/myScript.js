c622a0aa225d03227fbd9f78c40f249ebf6882a9
ANSHUL JAISWAL <69888501+anshuljaiswal428@users.noreply.github.com>
2024-05-14 20:55:09 +0530

    Add files via upload

---
+++ QUIZ 1 QUESTIONS/myScript.js
@@ -0,0 +1,20 @@
+function myAdd() {
+    let n1 = parseInt(document.getElementById('num1').value);
+    let n2 = parseInt(document.getElementById('num2').value);
+    alert("ADDITION OF "+n1+" & "+n2+" IS "+(n1+n2));
+}
+function mySub() {
+    let n1 = parseInt(document.getElementById('num1').value);
+    let n2 = parseInt(document.getElementById('num2').value);
+    alert("SUBTRACTION OF "+n1+" & "+n2+" IS "+(n1-n2));
+}
+function myMul() {
+    let n1 = parseInt(document.getElementById('num1').value);
+    let n2 = parseInt(document.getElementById('num2').value);
+    alert("MULTIPLICATION OF "+n1+" & "+n2+" IS "+(n1*n2));
+}
+function myDiv() {
+    let n1 = parseInt(document.getElementById('num1').value);
+    let n2 = parseInt(document.getElementById('num2').value);
+    alert("DIVISION OF "+n1+" & "+n2+" IS "+(n1/n2));
+}
